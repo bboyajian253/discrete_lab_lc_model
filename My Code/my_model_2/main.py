f44243dd2f77fc57df9458dd3bb7409c1b573a0d
benb253 <88692666+benb253@users.noreply.github.com>
2024-05-31 12:05:08 -0500

    Add main.py and solver.py to model 2

---
+++ My Code/my_model_2/main.py
@@ -0,0 +1,12 @@
+"""
+My Model 2 - main file
+
+Author: Ben Boyajian
+Date: 2024-05-31 11:38:38
+"""
+import pars_shocks_and_wages as ps
+import model_no_uncert as model
+import my_toolbox as tb
+
+def main():
+    myPars = ps.Pars()
---
+++ My Code/my_model_2/solver.py
@@ -0,0 +1,18 @@
+"""
+solver.py
+
+This file contains the solver module for the project.
+
+Author: Ben Boyajian
+Date: 2024-05-31 11:42:26
+"""
+import numpy as np
+import model_no_uncert as model
+
+from pars_shocks_and_wages import Pars
+from numba import njit, prange, float64
+
+def solv_lc( myPars: Pars)  :
+    #start status csv
+    pass
+def solve_per_j( myPars: Pars)
