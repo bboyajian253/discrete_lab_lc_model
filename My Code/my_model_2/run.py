bdc47cfc67600d1e030383ca47ada1658bfaeece
benb253 <b.boyajian@vanderbilt.edu>
2024-06-19 14:52:14 -0500

    Minor update to run, rename calibrate to calibration

--- My Code/my_model_2/run.py
+++ My Code/my_model_2/run.py
@@ -12,16 +12,25 @@ Date: 2024-06-19 12:07:5
 # General
 import numpy as np
 import time
+from typing import List, Dict
 
 # My code
 import my_toolbox as tb
 import solver
 import old_simulate as simulate
-import calibrate as calib
+import calibration as calib
 import plot_lc as plot_lc
 from pars_shocks_and_wages import Pars, Shocks
 
 # Run the model
+def run_model(myPars: Pars, myShocks: Shocks)-> List[Dict[str, float]]:
+    #If solve, solve the model
+    #always load state specific solutions
+    #if no_calibrate_but_sim, simulate without calibrating
+    #always load simulated life cycles
+    #if output, output the results
 
-# Print the parameters
-# Output the results and the associated graphs
+def output(myPars: Pars, state_sols: Dict[str, np.ndarray], sim_lc: Dict[str, np.ndarray])-> None:
+    # Print parameters
+    # Output the results and the associated graphs
+    pass
