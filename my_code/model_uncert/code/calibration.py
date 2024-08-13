fixed after being broken: corrected simulation weighting to reflect the new state var/perm H type.

my_code/model_uncert/code/calibration.py
@@ -266,13 +266,13 @@ def alpha_moment_giv_alpha(myPars : Pars, main_path : str, new_alpha: float) ->T
 
 def alpha_moment_giv_sims(myPars: Pars, sims: Dict[str, np.ndarray])-> float:
     labor_sims = sims['lab'][:,:,:,:,:myPars.J]
-    print(f"labor_sims mean = {np.mean(labor_sims)}")
+    # print(f"labor_sims mean = {np.mean(labor_sims)}")
     weighted_labor_sims = model.gen_weighted_sim(myPars, labor_sims)
-    print(f"weighted_labor_sims mean = {np.mean(weighted_labor_sims)}")
+    # print(f"weighted_labor_sims mean = {np.mean(weighted_labor_sims)}")
     mean_lab_by_age_and_sim = np.sum(weighted_labor_sims, axis = tuple(range(weighted_labor_sims.ndim-2)))
-    print(f"mean_lab_by_age_and_sim = {mean_lab_by_age_and_sim}")
+    # print(f"mean_lab_by_age_and_sim = {mean_lab_by_age_and_sim}")
     mean_lab = np.mean(mean_lab_by_age_and_sim)
-    print(f"mean labor worked = {mean_lab}")
+    # print(f"mean labor worked = {mean_lab}")
     return mean_lab
 
 def get_alpha_targ(myPars: Pars) -> float:

my_code/model_uncert/code/main.py
@@ -44,7 +44,7 @@ def main_1():
     myPars = Pars(main_path, J=51, a_grid_size=501, a_min= -100.0, a_max = 100.0, H_grid=np.array([0.0, 1.0]), H_weights=np.array([0.5, 0.5]),
                 nu_grid_size=1, alpha = 0.45, sim_draws=1000, lab_FE_grid = my_lab_FE_grid, lab_FE_weights = my_lab_FE_weights,
                 wage_coeff_grid = w_coeff_grid, max_iters = 100, max_calib_iters = 100, sigma_util = 0.9999,
-                print_screen=1)
+                print_screen=0)
     myShocks = Shocks(myPars)
     myPars.path = main_path
     sols, sims =run.run_model(myPars, myShocks, solve = True, calib = True, sim_no_calib = False, 

my_code/model_uncert/code/model_uncert.py
@@ -299,7 +299,7 @@ def gen_weighted_sim(myPars: Pars, lc_moment_sim: np.ndarray) -> np.ndarray:
     weighted_sim = lc_moment_sim * my_sim_weights_reshaped
     # since the weights are calculated only with the labor_fe in mind, we need to divide by the number of health states
     # else weights essentially sum to 2 instead of 1
-    weighted_sim = weighted_sim / myPars.H_grid_size
+    weighted_sim = weighted_sim / (myPars.H_grid_size*myPars.H_type_perm_grid_size)
     return weighted_sim
 
 @njit